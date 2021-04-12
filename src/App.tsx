ef3507459b85155443ae5648f6e951bf7fea2e60
Cristian <cristianlevrero@gmail.com>
2021-04-12 10:21:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.scss';
+
+function App() {
+  return (
+    
+    <section className="song">
+      <head>
+        <h1>Tu Amor</h1>
+        <p>chord layout test</p>
+      </head>
+      <div>
+        <p>Yo quise el fin<span className="chord"><span>A/G#</span></span> y había mas, yo quise más<span className="chord"><span>A/G#</span></span> no había fin.</p>
+        <p>Lo que yo qu<span className="chord"><span>A#</span></span>ise encontrar estaba at<span className="chord"><span>A#</span></span>rás y no aquí.</p>
+        <p>Desde las som<span className="chord"><span>Bm7</span></span>bras no v<span className="chord"><span>C#m7</span></span>i las som<span className="chord"><span>Bm7</span></span>bras y  n<div className="chord"><span>C#m7</span></div>o  vi l<div className="chord"><span>Dmaj7</span></div>uz.</p>
+      </div>
+      <p>No voy a llorar si nadie me acompaña,</p>
+      <p>no voy a dejar ni un camino sin andar.</p>
+      
+    </section>
+
+    
+  );
+}
+
+export default App;
